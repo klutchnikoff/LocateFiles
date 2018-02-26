f6d0c35579b2a33fa95998f5cc2fc6df4b6ad05a
Nicolas Klutchnikoff <nicolas.klutchnikoff@gmail.com>
2018-02-26 14:32:16 +0100

    Use "$" to complete a query
    This avoids frequent requests and allows global acceleration

--- locatefiles/__init__.py
+++ locatefiles/__init__.py
@@ -7,26 +7,32 @@ from subprocess import run, PIPE
 
 class LocateFiles(DirectoryPaneCommand):
 
-	def __call__(self):
+	def __call__(self, use_regex=False):
 		result = show_quicksearch(self._get_items)
 		if result:
 			query, value = result
-			value = value.decode("utf-8")
-			value = as_url(value)
-			self.pane.set_path(dirname(value))
-			#self.pane.place_cursor_at(value)
-		else:
-			return
+			if value:
+				value = value.decode("utf-8")
+				value = as_url(value)
+				self.pane.set_path(dirname(value))
+				#self.pane.place_cursor_at(value)
 
 	def _get_items(self, query):
-		located_files = run(self._locate(query), stdout=PIPE)
-		located_files = located_files.stdout.split()
-		for filep in located_files:
-			yield QuicksearchItem(filep)
+		if query.endswith("$"):
+			query = query[:-1]
+			located_files = run(self._locate(query), stdout=PIPE)
+			located_files = located_files.stdout.split()
+			for filep in located_files:
+				yield QuicksearchItem(filep)
+		else:
+			yield QuicksearchItem(
+				None,
+				title="Use $ to end your query", 
+				description="The number of displayed files is limited to 50. The query can contain globbling characters.")
 
 	def _locate(self, query):
 		if PLATFORM == 'Linux':
-			return ['/usr/bin/locate', '-l10', query]
-		#elif PLATFORM == 'Mac':
-		#	return ['/usr/bin/mdfind', '-name ', query]
+			return ['/usr/bin/locate', '-l50', query]
+		elif PLATFORM == 'Mac':
+			return ['/usr/bin/mdfind', '-name', query]
 		raise NotImplementedError(PLATFORM)
